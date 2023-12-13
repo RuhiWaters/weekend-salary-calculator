f9053dd73c43fc9b7950179ac964c5a20b04f704
Ru <edel.m.waters@gmail.com>
2023-12-13 14:17:09 -0600

    attempt to resolve commit/push issues-re-cloned

---
+++ client.js
@@ -0,0 +1,49 @@
+console.log("In client.js");
+
+let totalMonthlyCost = 0;
+
+//input values
+function submitForm() {
+  let firstName = document.querySelector("#firstName").value;
+  let lastName = document.querySelector("#lastName").value;
+  let idNumber = document.querySelector("#idNumber").value;
+  let jobTitle = document.querySelector("#jobTitle").value;
+  let annualSalary = parseFloat(document.querySelector("#annualSalary").value);
+
+  let monthly = annualSalary / 12;
+  totalMonthlyCost += monthly;
+
+  let tableBody = document.querySelector("#employeeTblbd");
+  tableBody.innerHTML += `<tr><td>${firstName}</td><td>${lastName}</td><td>${idNumber}</td><td>${jobTitle}</td><td>${annualSalary}</td><td><button onclick='deleteEmployee(event)'>Delete</button></td></tr>`;
+
+  //update table
+  updateFooter();
+
+  //clear input
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("idNumber").value = "";
+  document.getElementById("jobTitle").value = "";
+  document.getElementById("annualSalary").value = "";
+}
+
+function deleteEmployee(event) {
+  //remover table row
+  event.target.parentElement.parentElement.remove();
+
+  //update footer after deleting
+  updateFooter();
+}
+
+function updateFooter() {
+  //update the footer with total monthly cost
+  let footer = document.querySelector("#ft");
+  footer.innerHTML = `Monthly Total: $ ${totalMonthlyCost}`;
+
+  //if over budget else
+  if (totalMonthlyCost > 20000) {
+    footer.classList.add("over-budget");
+  } else {
+    footer.classList.remove("over-budget");
+  }
+}
